89d48d5d819a88a8230d565bd8d3706e512e1ff7
Patrick White <byelipk@kso2.local>
2017-07-16 10:36:33 -0400

    Display rentals

--- app/models/rental.js
+++ app/models/rental.js
@@ -2,5 +2,8 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name:      DS.attr('string'),
-  dailyRate: DS.attr('string')
+  dailyRate: DS.attr('string'),
+  imgUrl:    DS.attr('string'),
+
+  bookings: DS.hasMany('booking')
 });
