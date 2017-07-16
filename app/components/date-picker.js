13b74d46fa768001b1ad040195bf69bd15583432
Patrick White <byelipk@kso2.local>
2017-07-15 22:09:08 -0400

    Display selected date range in search from

--- app/components/date-picker.js
+++ app/components/date-picker.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
+  moment: Ember.inject.service(),
+
   calculatePosition(trigger) {
     const element = document.querySelector("#date-picker");
     const measurements = element.getBoundingClientRect();
@@ -12,5 +15,20 @@ export default Ember.Component.extend({
         width: measurements.width * 3
       }
     };
-  }
+  },
+
+  yesterday: moment().add(-1, 'days'),
+
+  formattedRange: Ember.computed('range', function() {
+    const range = this.get('range');
+
+    if (!range || !range.start || !range.end) {
+      return 'Anytime';
+    }
+
+    const start = range.start.format("MMM DD");
+    const end = range.end.format("MMM DD");
+
+    return `${start} - ${end}`;
+  })
 });
