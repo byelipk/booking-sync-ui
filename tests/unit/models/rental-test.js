0af217a9b21361c945a6e1d50b33e50220a56637
Patrick White <byelipk@kso2.local>
2017-07-19 12:43:52 -0400

    Test hasMany rel from rental -> booking

--- tests/unit/models/rental-test.js
+++ tests/unit/models/rental-test.js
@@ -1,12 +1,15 @@
+import Ember from 'ember';
 import { moduleForModel, test } from 'ember-qunit';
 
 moduleForModel('rental', 'Unit | Model | rental', {
   // Specify the other units that are required for this test.
-  needs: []
+  needs: ['model:booking']
 });
 
-test('it exists', function(assert) {
-  let model = this.subject();
-  // let store = this.store();
-  assert.ok(!!model);
+test('should have many bookings', function(assert) {
+  const Rental = this.store().modelFor('rental');
+  const relationship = Ember.get(Rental, 'relationshipsByName').get('bookings');
+
+  assert.equal(relationship.key, 'bookings', 'has relationship with booking');
+  assert.equal(relationship.kind, 'hasMany', 'kind of relationship is hasMany');
 });
