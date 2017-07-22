2fd623cc1a17c1555ad72154aceda8c112e0a120
Patrick White <byelipk@kso2.local>
2017-07-22 10:14:40 -0400

    Test booking-card interaction

--- app/components/booking-card.js
+++ app/components/booking-card.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['col-12', 'col-md-6', 'col-lg-3']
+  classNames: ['col-12', 'col-md-6', 'col-lg-3'],
+
+  actions: {
+    delete() {
+      this.get('onDelete')(this.get('booking'));
+    },
+    update() {
+      this.get('onUpdate')(this.get('booking'));
+    }
+  }
 });
--- tests/integration/components/booking-card-test.js
+++ tests/integration/components/booking-card-test.js
@@ -1,5 +1,21 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
+import Ember from 'ember';
+
+// Setup data
+const now = moment();
+
+const Rental = Ember.Object.extend({
+  name: "My booking"
+});
+
+const Booking = Ember.Object.extend({
+  rental: Rental.create(),
+  startAt: now,
+  endAt: now.add(1, 'days'),
+  clientEmail: "hello@world.com"
+});
 
 moduleForComponent('booking-card', 'Integration | Component | booking card', {
   integration: true
@@ -9,17 +25,37 @@ test('it renders', function(assert) {
 
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('booking', Booking.create());
 
-  this.render(hbs`{{booking-card}}`);
+  this.render(hbs`{{booking-card booking=booking}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('.card-title').text().trim(), 'My booking');
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#booking-card}}
-      template block text
-    {{/booking-card}}
-  `);
+test('delete action works', function(assert) {
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.set('externalAction', (booking) => {
+    assert.equal(booking, this.get('booking'),
+      "Expected to receive booking to delete.");
+  });
+
+  this.set('booking', Booking.create());
+
+  this.render(hbs`{{booking-card booking=booking onDelete=(action externalAction)}}`);
+
+  this.$('button:first-child').click();
+});
+
+test('update action works', function(assert) {
+
+  this.set('externalAction', (booking) => {
+    assert.equal(booking, this.get('booking'),
+      "Expected to receive booking to update.");
+  });
+
+  this.set('booking', Booking.create());
+
+  this.render(hbs`{{booking-card booking=booking onUpdate=(action externalAction)}}`);
+
+  this.$('button:last-child').click();
 });
