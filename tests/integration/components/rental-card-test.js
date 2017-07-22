84ea836d8dbf5c48769568442d4a0c167fbbe2ca
Patrick White <byelipk@kso2.local>
2017-07-22 10:26:47 -0400

    Test rendering of rental-card

--- tests/integration/components/rental-card-test.js
+++ tests/integration/components/rental-card-test.js
@@ -1,25 +1,24 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+
+const Rental = Ember.Object.extend({
+  name: "My booking",
+  dailyRate: 120
+});
 
 moduleForComponent('rental-card', 'Integration | Component | rental card', {
   integration: true
 });
 
 test('it renders', function(assert) {
+  assert.expect(3);
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('rental', Rental.create());
 
-  this.render(hbs`{{rental-card}}`);
+  this.render(hbs`{{rental-card rental=rental}}`);
 
-  assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(hbs`
-    {{#rental-card}}
-      template block text
-    {{/rental-card}}
-  `);
-
-  assert.equal(this.$().text().trim(), 'template block text');
+  assert.equal(this.$('strong').text().trim(), '$120');
+  assert.ok(this.$('p').text().match(/My booking/));
+  assert.equal(this.$('img').attr('alt').trim(), 'My booking');
 });
