904a8200bcefc1d34ed73dd57cf787db8f477740
Patrick White <byelipk@kso2.local>
2017-07-22 18:33:18 -0400

    Test application serializer

--- tests/unit/serializers/application-test.js
+++ tests/unit/serializers/application-test.js
@@ -1,15 +1,33 @@
 import { moduleForModel, test } from 'ember-qunit';
+import { startMirage } from 'client/initializers/ember-cli-mirage';
 
-moduleForModel('application', 'Unit | Serializer | application', {
-  // Specify the other units that are required for this test.
-  needs: ['serializer:application']
+moduleForModel('booking', 'Unit | Serializer | application', {
+  needs: [
+    'serializer:application',
+    'adapter:application',
+    'transform:utc',
+    'model:rental'
+  ],
+
+  beforeEach() {
+    this.server = startMirage();
+  },
+
+  afterEach() {
+    this.server.shutdown();
+  }
 });
 
-// Replace this with your real tests.
-test('it serializes records', function(assert) {
-  let record = this.subject();
+test('it serializes bookings', function(assert) {
+  return this.store().findAll('booking').then(bookings => {
+    assert.equal(bookings.get('length'), 1,
+      `Expected 1 booking, but there were ${bookings.get('length')}`);
+  });
+});
 
-  let serializedRecord = record.serialize();
-
-  assert.ok(serializedRecord);
+test('it serializes rentals', function(assert) {
+  return this.store().findAll('rental').then(rentals => {
+    assert.equal(rentals.get('length'), 12,
+      `Expected 12, but there were ${rentals.get('length')}`);
+  });
 });
