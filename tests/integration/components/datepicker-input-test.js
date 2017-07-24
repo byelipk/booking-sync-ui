962b64f35e6b03346d147e516c865bd4af1ac1ab
Patrick White <byelipk@kso2.local>
2017-07-24 13:11:16 -0400

    Test datepicker input

--- tests/integration/components/datepicker-input-test.js
+++ tests/integration/components/datepicker-input-test.js
@@ -1,25 +1,37 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+
+import moment from 'moment';
+
+const Rental = Ember.Object.extend({
+  name: "My Rental",
+  dailyRate: "100"
+});
 
 moduleForComponent('datepicker-input', 'Integration | Component | datepicker input', {
   integration: true
 });
 
-test('it renders', function(assert) {
+test('it can render calendar in a dropdown', function(assert) {
+  this.set('dropdown', true);
+  this.set('rental', Rental.create());
+  this.set('range', { start: moment(), end: moment().add(1, 'day') });
 
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.render(hbs`{{datepicker-input range=range dropdown=dropdown}}`);
 
-  this.render(hbs`{{datepicker-input}}`);
+  assert.equal(this.$('.ember-basic-dropdown-trigger').length, 1,
+    "Expected an input field.");
 
-  assert.equal(this.$().text().trim(), '');
+  assert.equal(this.$('.calendar').length, 0, "Did not expect to find a calendar");
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#datepicker-input}}
-      template block text
-    {{/datepicker-input}}
-  `);
+test('it can render inline', function(assert) {
+  this.set('dropdown', false);
+  this.set('rental', Rental.create());
+  this.set('range', { start: moment(), end: moment().add(1, 'day') });
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.render(hbs`{{datepicker-input range=range dropdown=dropdown}}`);
+
+  assert.equal(this.$('.calendar').length, 1, "Expected to render a calendar.");
 });
