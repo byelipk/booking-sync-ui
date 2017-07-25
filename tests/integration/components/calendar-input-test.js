c9dfe2d3614d874c6ffada61b6b4421858896a0a
Patrick White <byelipk@kso2.local>
2017-07-24 23:32:43 -0400

    Refactor calendar input test

--- tests/integration/components/calendar-input-test.js
+++ tests/integration/components/calendar-input-test.js
@@ -1,9 +1,7 @@
 import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
-// import wait from 'ember-test-helpers/wait';
 import moment from 'moment';
-// import { find } from "ember-native-dom-helpers";
 
 const Rental = Ember.Object.extend({
   name: "My rental",
@@ -23,26 +21,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{calendar-input rental=rental range=range}}`);
 
   assert.equal(this.$('.ember-power-calendar').length, 1);
-});
-
-test('making a selection invokes `updateRange`', function(assert) {
-  assert.expect(1);
-
-  this.set('rental', Rental.create());
-  this.set('range', {start: moment(), end: moment().add(5, 'days')});
-
-  const start = this.get('range.end').clone().add(1, 'day');
-
-  this.on('myUpdate', function handler(selection) {
-    assert.equal(selection.moment.start.format("YYYY-MM-DD"), start.format("YYYY-MM-DD"));
-  });
-
-  this.render(hbs`
-    {{calendar-input
-      rental=rental
-      range=range
-      onSelect=(action 'myUpdate')}}`);
-
-
-  this.$(`button.ember-power-calendar-day[data-date='${start.format("YYYY-MM-DD")}']`).click();
 });
