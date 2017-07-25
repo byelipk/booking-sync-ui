749cf3bc168490e7af78c122fd1a9959d63f9f54
Patrick White <byelipk@kso2.local>
2017-07-25 09:49:47 -0400

    Mirage now uses rental fixtures

--- mirage/fixtures/rental.js
+++ mirage/fixtures/rental.js
@@ -1,3 +1,74 @@
 export default [
-  
+  {
+    "id": "1",
+    "name": "Kayacking in the Colorado River",
+    "dailyRate": "2000.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/e7a377a9-9a07-41af-8a21-cf933c29b791.jpg?aki_policy=poster"
+  },
+  {
+    "id": "2",
+    "name": "Helicopter Ride",
+    "dailyRate": "1250.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/03ee2ce2-782e-42f3-b3c2-7cf85b3cd6c9.jpg?aki_policy=poster"
+  },
+  {
+    "id": "3",
+    "name": "Library Visit",
+    "dailyRate": "2.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/707a7efa-90e2-440e-88a8-916d1f291ede.jpg?aki_policy=poster"
+  },
+  {
+    "id": "4",
+    "name": "Red Sox Game",
+    "dailyRate": "15.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/56d23756-b70b-440c-a081-5ccc24a6c047.jpg?aki_policy=poster"
+  },
+  {
+    "id": "5",
+    "name": "Dance with the Ice Queen",
+    "dailyRate": "500.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/aa0cf07e-07dc-4ca5-ac88-73b9997f7b80.jpg?aki_policy=poster"
+  },
+  {
+    "id": "6",
+    "name": "Walk to the Holywood Hills",
+    "dailyRate": "50.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/ee528d90-e9bf-4c9d-9dde-9150b2956e38.jpg?aki_policy=poster"
+  },
+  {
+    "id": "7",
+    "name": "Learn to cook Japanese food",
+    "dailyRate": "25.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/6acf36e0-cf9a-4433-9339-26d7b4e8e18a.jpg?aki_policy=poster"
+  },
+  {
+    "id": "8",
+    "name": "Hobbit Mansion",
+    "dailyRate": "1150.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/15525b73-c222-44c6-b1f8-483c6d049d73.jpg?aki_policy=poster"
+  },
+  {
+    "id": "9",
+    "name": "The White House",
+    "dailyRate": "1.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/b11cafba-6e13-4584-ba2a-243cfa28c160.jpg?aki_policy=poster"
+  },
+  {
+    "id": "10",
+    "name": "Clifton Beach Apartment",
+    "dailyRate": "175.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/d0b28e17-7c02-403b-8a9a-ae6c3cb61f5e.jpg?aki_policy=poster"
+  },
+  {
+    "id": "11",
+    "name": "Home cooked meal",
+    "dailyRate": "350.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/1e172922-0a58-4641-8700-2b65709b4734.jpg?aki_policy=poster"
+  },
+  {
+    "id": "12",
+    "name": "Juicy Box",
+    "dailyRate": "20.0",
+    "imgUrl": "https://a0.muscache.com/im/pictures/1e172922-0a58-4641-8700-2b65709b4734.jpg?aki_policy=poster"
+  }
 ];
